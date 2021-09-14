49cc2975e9db6c7aebf8da73fb92765515444fb2
Jayme Coulton <coultonjayme@yahoo.com>
2021-09-14 11:26:00 -0500

    updated

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ let rollbar = new Rollbar({
 const students = [];
 const app = express();
 app.use(express.json());
-app.use("/style", express.static("/public/styles.css"));
+app.use("/style", express.static("./public/styles.css"));
 
 app.get("/", (req, res) => {
 	res.sendFile(path.join(__dirname, "/public/index.html"));
