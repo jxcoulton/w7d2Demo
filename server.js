f46769ce84949adc4725b55835431abf8ab677a8
Jayme Coulton <coultonjayme@yahoo.com>
2021-09-14 11:15:38 -0500

    updated

--- server.js
+++ server.js
@@ -21,13 +21,23 @@ app.get("/", (req, res) => {
 app.post("/api/student", (req, res) => {
 	let { name } = req.body;
 	name = name.trim();
-	students.push(name);
 
-	rollbar.log("Student added successfully", {
-		author: "Jayme",
-		type: "Manual entry",
-	});
-	res.status(200).send(students);
+	const index = students.findIndex((studentName) => studentName === name);
+
+	if (index === -1 && name !== "") {
+		students.push(name);
+		rollbar.log("Student added successfully", {
+			author: "Scott",
+			type: "manual entry",
+		});
+		res.status(200).send(students);
+	} else if (name === "") {
+		rollbar.error("No name given");
+		res.status(400).send("must provide a name.");
+	} else {
+		rollbar.error("student already exists");
+		res.status(400).send("that student already exists");
+	}
 });
 
 const port = process.env.PORT || 4545;
