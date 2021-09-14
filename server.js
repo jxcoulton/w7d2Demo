b9ef3a0323f99d56b78eaae75ca27931acf4cf44
Jayme Coulton <coultonjayme@yahoo.com>
2021-09-14 10:21:53 -0500

    rollbar added

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const path = require("path");
+const Rollbar = require("rollbar");
+
+let rollbar = new Rollbar({
+	accesstoken: "8743a46fe63242a1aa6cf28e86fc5130",
+	captureUncaught: true,
+	captureUnhandledRejections: true,
+});
 
 const app = express();
 
 app.get("/", (req, res) => {
 	res.sendFile(path.join(__dirname, "/public/index.html"));
+	rollbar.info("HTML file served successfully!");
 });
 
 const port = process.env.PORT || 4545;
